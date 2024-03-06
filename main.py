257879932dee2fc2a10e9eff9afe006a6953d636
Ho Nyeong <dev.honing@gmail.com>
2024-03-06 18:33:19 +0900

    Feat: 이미지 회전(180도) 구현 및 저장

--- main.py
+++ main.py
@@ -3,27 +3,16 @@ import cv2
 # 이미지 불러오기
 img = cv2.imread('lake.jpg')
 
-# 그레이스케일로 변환
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 이미지 회전
+# 이미지 중심점 구하기
+(height, width) = img.shape[:2]
+center = (width / 2, height / 2)
 
-# # 이미지 확인(새 창에서 열기)
-# cv2.imshow('Original image', img)
-# cv2.imshow('Gray image', gray)
+# 180도 회전 매트릭스를 생성
+M = cv2.getRotationMatrix2D(center, 180, 1.0)
 
-# # 키보드 입력으로 닫기
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# 회전 매트릭스를 이미지에 적용
+rotated = cv2.warpAffine(img, M, (width, height))
 
-# 변환된 이미지 확인
-# cv2.imshow('gray_lake.jpg', gray)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# 변환된 이미지 저장
-cv2.imwrite('gray_lake.jpg', gray)
-
-# 이미지 자르기
-cropped = img[0:100, 0:100]
-
-# 자른 이미지 저장
-cv2.imwrite('cropped_lake.jpg', cropped)
+# 회전된 이미지 저장
+cv2.imwrite('rotated.jpg', rotated)
