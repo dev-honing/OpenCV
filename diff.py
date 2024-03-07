4793a56f51e60800541770106ad153c1ea509b6b
Ho Nyeong <dev.honing@gmail.com>
2024-03-07 10:47:51 +0900

    Feat: 평탄화 및 새로운 JSON 파일로 저장

--- diff.py
+++ diff.py
@@ -1,4 +1,5 @@
 import json
+import itertools
 import matplotlib.pyplot as plt
 
 # JSON 파일 읽기
@@ -8,10 +9,9 @@ with open('color_codes.json', 'r') as f:
 with open('gray_color_codes.json', 'r') as f:
     gray_color_codes = json.load(f)
     
-# 리스트 길이 확인
-print(len(color_codes)) # 19961856
-print(len(gray_color_codes)) # 19961856
+# 평탄화 작업
+color_codes_flat = list(itertools.chain.from_iterable(color_codes))
 
-# 각 리스트의 첫 번째 요소의 타입 확인
-print(type(color_codes[0])) # <class 'list'>
-print(type(gray_color_codes[0])) # <class 'int'>
+# 평탄화된 리스트를 새로운 JSON 파일로 저장
+with open('color_codes_flat.json', 'w') as f:
+    json.dump(color_codes_flat, f)
