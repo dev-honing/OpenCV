a9f8e6f402af10d1887b365998addb17683e57bc
Ho Nyeong <dev.honing@gmail.com>
2024-03-07 13:16:51 +0900

    Feat: 해결 기념으로 JAVA를 이용해 콘솔 출력

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, JAVA!");
+  }
+}
+
+// 이것은 주석
+
+// 실행법 정리
+// 1. 오라클에서 JAVA, JDK 설치
+// 2. javac -version으로 컴파일 테스트
+// 3. javac Main.java로 컴파일
+// 4. java Main 명령으로 터미널에 콘솔 출력
