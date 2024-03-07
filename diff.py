67ef0b80367f61006251c03cfc1397c768453c8b
Ho Nyeong <dev.honing@gmail.com>
2024-03-07 10:54:52 +0900

    Test: 평탄화된 리스트의 길이와 타입 확인

--- diff.py
+++ diff.py
@@ -3,15 +3,16 @@ import itertools
 import matplotlib.pyplot as plt
 
 # JSON 파일 읽기
-with open('color_codes.json', 'r') as f:
-    color_codes = json.load(f)
+with open('color_codes_flat.json', 'r') as f:
+    color_codes_flat =  json.load(f)
     
 with open('gray_color_codes.json', 'r') as f:
     gray_color_codes = json.load(f)
-    
-# 평탄화 작업
-color_codes_flat = list(itertools.chain.from_iterable(color_codes))
 
-# 평탄화된 리스트를 새로운 JSON 파일로 저장
-with open('color_codes_flat.json', 'w') as f:
-    json.dump(color_codes_flat, f)
+# 각 리스트의 길이 확인
+print(len(color_codes_flat)) # 59885568
+print(len(gray_color_codes)) # 19961856
+
+# 각 리스트의 첫 번째 요소의 타입 확인
+print(type(color_codes_flat[0])) # <class 'int'>
+print(type(gray_color_codes[0])) # <class 'int'>
