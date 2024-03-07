b8a2eb59f1e71b30a61459dfff549d9c83318bdb
Ho Nyeong <dev.honing@gmail.com>
2024-03-07 10:41:32 +0900

    Feat: 각 리스트의 타입 확인 및 출력값 주석 표기
    - color_codes: 2차원 리스트로 구성됨
    - gray_color_codes: 1차원 리스트로 구성됨
    -> 평탄화 작업이 필요한 상황

--- diff.py
+++ diff.py
@@ -11,3 +11,7 @@ with open('gray_color_codes.json', 'r') as f:
 # 리스트 길이 확인
 print(len(color_codes)) # 19961856
 print(len(gray_color_codes)) # 19961856
+
+# 각 리스트의 첫 번째 요소의 타입 확인
+print(type(color_codes[0])) # <class 'list'>
+print(type(gray_color_codes[0])) # <class 'int'>
